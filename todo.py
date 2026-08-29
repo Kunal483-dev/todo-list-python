b5c3a6899f4177f954c01538c4fdba861d00ca1d
Kunal483-dev <rajpootkunal969@gmail.com>
2026-08-29 15:23:02 +0530

    Update todo.py
    Added initial features to the application

--- todo.py
+++ todo.py
@@ -1,1 +1,10 @@
-print("To-Do List Application")
+print("===== TO-DO LIST =====")
+
+print("1. Add Task")
+print("2. View Tasks")
+print("3. Remove Task")
+print("4. Exit")
+
+choice = input("Enter your choice: ")
+
+print("You selected:", choice)
